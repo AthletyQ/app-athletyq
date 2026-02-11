8a03c492611b15a40732f75b85c8023baca58118
abisheka-oss <abisheka.20232309@iit.ac.lk>
2026-02-11 21:04:37 +0530

    initial commit

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,14 +1,17 @@
-import React from "react";
-
-type AuthLayoutProps = {
+export default function SignupLayout({
+  children,
+}: {
   children: React.ReactNode;
-};
-
-export default function AuthLayout({ children }: AuthLayoutProps) {
+}) {
   return (
-    <div>
+    <html lang="en">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
+      </head>
+      <body className="antialiased">
         {children}
-    </div>
+      </body>
+    </html>
   );
 }
 
