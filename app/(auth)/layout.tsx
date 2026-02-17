75d72644d7a7bdbc9b92be653c5587b9eb228d34
Janul69 <januldissanayake99@gmail.com>
2026-02-17 21:51:54 +0530

    fixed login

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,14 +4,9 @@ export default function SignupLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <head>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
-      </head>
-      <body className="antialiased">
-        {children}
-      </body>
-    </html>
+    <div className="antialiased">
+      {children}
+    </div>
   );
 }
 
